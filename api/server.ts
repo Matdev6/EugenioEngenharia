9bdfa9cee707bb098e32084c539ee2e559f01d9e
Matdev6 <mateusousa123@hotmail.com>
2025-07-23 10:44:49 -0300

    fix fix
    fix

---
+++ api/server.ts
@@ -0,0 +1,16 @@
+import { renderPage } from 'vike/server'
+import type { IncomingMessage, ServerResponse } from 'http'
+
+export default async function handler(req: IncomingMessage, res: ServerResponse) {
+    const pageContext = await renderPage({ urlOriginal: req.url! })
+
+    if (!pageContext.httpResponse) {
+        res.statusCode = 500
+        res.end('Internal Server Error')
+        return
+    }
+
+    const { body, statusCode, headers } = pageContext.httpResponse
+    res.writeHead(statusCode, Object.fromEntries(headers))
+    res.end(body)
+}
