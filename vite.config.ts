8889d5e22873cb7ad6161767319ac9e9b0499db6
Matdev6 <mateusousa123@hotmail.com>
2025-07-23 16:37:17 -0300

    teste
    teste

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite-plugin'
+import tailwindcss from '@tailwindcss/vite'
 import vike from 'vike/plugin'
 
 export default defineConfig({
